66dd5d8c0815a1b3cbeb115cbdbb2658de08d4b1
ls125781003 <125781003@qq.com>
2023-06-20 13:13:30 +0800

    Update Route.js

--- v/Route.js
+++ v/Route.js
@@ -1,7 +1,7 @@
 
 eval(fetch('hiker://files/cache/fileLinksᴰⁿ.txt'));
 var Route = {
-    jxLazy: "require('./v/Route.js');aytmParse(input)",
+    jxLazy: "require('https://raw.githubusercontent.com/ls125781003/dcbf/m/v/Route.js');aytmParse(input)",
     setParse: function() {
         eval(fetch('hiker://files/cache/fileLinksᴰⁿ.txt'));
         if (!getVar('jxItemV')) {
