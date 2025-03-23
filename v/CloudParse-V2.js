60a6c20ef9269f1d853cbc4e70785dd41b61d865
Liu <18409283+ls125781003@users.noreply.github.com>
2025-03-23 17:15:45 +0800

    1
    fix

---
+++ v/CloudParse-V2.js
@@ -0,0 +1,177 @@
+var _0x7821 = [
+    'DnPage',
+    'fORgu',
+    'rlMQN',
+    'xFYyo',
+    'hiker://files/cache/Parse_Dn.html',
+    'movie_1',
+    'kiiew',
+    'var\x20yurl=\x27https://pic.cimtp.com/jiexi/jianghu.php?v=\x27+input;var\x20play=\x20request(yurl,{headers:{\x27Referer\x27:\x27https://pic.cimtp.com/jx/?url=\x27}}).match(/\x22url\x22:\x20\x22(.*?)\x22/)[1];play.indexOf(\x27mgtv\x27)>-1?play\x20+\x20\x27;{Referer@https://m.mgtv.com/}\x27:play',
+    'hiker://empty#',
+    'push',
+    'var\x20yurl=\x27https://pcvideoaliyun.tianshangrenjian.mgtv.com.88wuma.com/?url=\x27+input;var\x20vkey=request(yurl,{}).match(/vkey\x20=\x20\x27(.*?)\x27/)[1];var\x20play=JSON.parse(request(\x27https://pcvideoaliyun.tianshangrenjian.mgtv.com.88wuma.com/api_.php\x27,\x20{headers:{\x27content-type\x27:\x20\x27application/x-www-form-urlencoded\x27,\x27Referer\x27:yurl},\x20body:\x20\x27vkey=\x27+vkey+\x27&ip=117.150.20.222&dq=湖北省武昌市\x27,\x20method:\x20\x27POST\x27})).url;play.indexOf(\x27mgtv\x27)>-1?play\x20+\x20\x27;{Referer@https://m.mgtv.com/}\x27:play+\x27#.mp4\x27',
+    '\u3000\u3000脚本已同步最新版！规则更新维护在Q群或者论坛发布',
+    'AFYvJ',
+    'KhPPZ',
+    'LlXCO',
+    'https://gitee.com/Duan-Nian/Dn/raw/master/hikerview/CloudParse-V2_Dn.js',
+    'PXKvU',
+    'mySelect',
+    'JSON.parse(request(\x27https://www.x4jdm.tv/api.php\x27,\x20{\x20headers:\x20{\x20\x27content-type\x27:\x20\x27application/x-www-form-urlencoded\x27\x20},\x20body:\x20\x27url=\x27\x20+\x20input,\x20method:\x20\x27POST\x27\x20})).url',
+    'ahJCL',
+    'qiKRe',
+    'vGdCr',
+    'var\x20yurl=\x27https://www.521x5.com/jxjm/analysis.php?v=\x27+input;var\x20play\x20=\x20request(yurl,{headers:{\x27Referer\x27:\x27https://www.521x5.com/\x27}}).match(/urls\x20=\x20\x22(.*?)\x22/)[1];play.indexOf(\x27mgtv\x27)>-1?play\x20+\x20\x27;{Referer@https://titan.mgtv.com/}\x27:play+\x27#冷月.mp4\x27',
+    'true',
+    'var\x20play=JSON.parse(request(\x27https://1717.ntryjd.net/0526/api.php\x27,{body:\x27url=\x27+input,method:\x27POST\x27})).url;play.indexOf(\x27url=http\x27)>-1?decodeURIComponent(play.split(\x27url=\x27)[1]):play',
+    'var\x20yurl=\x27http://m3u8.tv.janan.net/json.php?url=\x27+input;var\x20play=JSON.parse(request(yurl,{})).url;var\x20plays=play.indexOf(\x27mgtv\x27)>-1?play\x20+\x20\x27;{Referer@http://m3u8.tv.janan.net}\x27:play;play.indexOf(\x27url=http\x27)>-1?decodeURIComponent(play.split(\x27url=\x27)[1]):plays',
+    'x5_webview_single',
+    'var\x20yurl=\x27https://foubin.com/qq.php?url=\x27+input;var\x20play=request(yurl,{headers:{\x27Referer\x27:\x27https://meijuchong.com/\x27}}).match(/url\x20=\x20\x27(.*?)\x27/)[1];play.indexOf(\x27mgtv\x27)>-1?play\x20+\x20\x27;{User-Agent@Mozilla/5.0\x20(Windows\x20NT\x2010.0\x5c；\x5c；\x20WOW64)\x20AppleWebKit/537.36\x20(KHTML,\x20like\x20Gecko)\x20Chrome/49.0.2623.87\x20Safari/537.36&&Referer@https://titan.mgtv.com}\x27:play+\x27#.mp4\x27',
+    'KuAXP',
+    'parse',
+    '点击进入论坛帖子列表',
+    'x5Route',
+    'hiker://files/cache/Parse_Dn.js',
+    'hiker://files/cache/MyParseSet.json',
+    'var\x20yurl=\x27https://jx.yunboys.cn/?url=\x27+input;eval(\x27var\x20json=\x27\x20+\x20request(yurl,\x20{headers:\x20{\x27Referer\x27:\x27https://jx.yunboys.cn/\x27}}).split(\x27post(\x22api.php\x22,\x27)[1].split(\x27,function\x27)[0].replace(/\x27other.*?\x5c)\x5c,/,\x27\x27).replace(\x27form\x27,\x271\x27));var\x20play=JSON.parse(request(\x27https://jx.yunboys.cn/api.php\x27,\x20{headers:{\x27Referer\x27:\x27https://jx.yunboys.cn/\x27},\x20body:\x20\x27url=\x27+json.url\x20+\x20\x27&referer=\x27+json.referer\x20+\x27&time=\x27+json.time\x20+\x27&type=&other=\x27+base64Encode(json.url)\x20+\x20\x27&ios=1&kys=\x27+json.kys,\x20method:\x27POST\x27})).url;play+\x27#isVideo=true#\x27;',
+    'hTbcg',
+    '@rule=js:var\x20res={};\x20var\x20d=[];var\x20jsUrl\x20=\x20fetch(\x27hiker://files/cache/MyParseSet.json\x27);if(jsUrl\x20==\x20\x27\x27\x20||\x20!fetch(JSON.parse(jsUrl).cj)){var\x20jsFile\x20=\x20fetch(input.split(\x27#\x27)[1]);}else{var\x20jsFile\x20=\x20fetch(JSON.parse(jsUrl).cj);}eval(jsFile);setParse();res.data=d;\x20setHomeResult(res);',
+    'nZnLi',
+    '455',
+    'x5cache',
+    'file:///storage/emulated/0/Android/data/com.example.hikerview/files/Documents/cache/Parse_Dn.html',
+    'cjcache',
+    'https://haikuoshijie.cn/user/1958',
+    'var\x20yurl=\x27https://m.dedyy.net/?url=\x27+input;var\x20play=request(yurl,{}).match(/url\x22:\x22(.*?)\x22/)[1];play.indexOf(\x27mgtv\x27)>-1?play\x20+\x20\x27;{Referer@https://m.mgtv.com/}\x27:play+\x27#.mp4\x27',
+    'MPTTU'
+];
+(function (_0x3e4d73, _0x782195) {
+    var _0x42e2fe = function (_0x1fab94) {
+        while (--_0x1fab94) {
+            _0x3e4d73['push'](_0x3e4d73['shift']());
+        }
+    };
+    _0x42e2fe(++_0x782195);
+}(_0x7821, 0x169));
+var _0x42e2 = function (_0x3e4d73, _0x782195) {
+    _0x3e4d73 = _0x3e4d73 - 0x0;
+    var _0x42e2fe = _0x7821[_0x3e4d73];
+    return _0x42e2fe;
+};
+var _mySet = {
+    'qy': '',
+    'yk': '',
+    'tx': '',
+    'mg': '',
+    'oth': '',
+    'qju': '默认',
+    'cjcache': _0x42e2('0x1f'),
+    'x5cache': _0x42e2('0x3'),
+    'cj': _0x42e2('0xe'),
+    'x5Url': 'https://gitee.com/Duan-Nian/Dn/raw/master/hikerview/Parse_Dn.html',
+    'x5Route': _0x42e2('0x27')
+};
+var mySelect = '\x20\x20\x20\x20\x20\x20\x20\x20<option>yun1717</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>parwix</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>全民</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>冷月</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>浆糊</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>cimtp</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>yunbo</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>JX123</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>eeeol</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>m3u8TV</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>https://jxx.smys8.cn/index.php?url=</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>https://vip.52jiexi.top/?url=</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>https://jx.ergan.top/?url=</option>\x20\x20\x20\x20\x20\x20\x20\x20<option>https://jx.m3u8.tv/jiexi/?url=</option>';
+var setUrl = _0x42e2('0x7') + _mySet['cj'] + _0x42e2('0x23');
+var set_switch = setUrl;
+var mode = fetch(_0x42e2('0x20'), {});
+var mySet = mode == '' ? _mySet : JSON[_0x42e2('0x1c')](mode);
+var eeeol = 'var\x20yurl=\x27http://video.eeeol.cn/jx.php?url=\x27+input;var\x20jxUrl\x20=\x20fetch(yurl,{}).split(\x22_url\x22)[2];var\x20play=jxUrl.match(/\x20=\x20\x27(.*?)\x27/)[1];play.indexOf(\x27url=http\x27)>-1?decodeURIComponent(play.split(\x27url=\x27)[1]):play';
+var yun1717 = _0x42e2('0x17');
+var cimtp = _0x42e2('0x6');
+var m3u8TV = _0x42e2('0x18');
+var 冷月 = _0x42e2('0x15');
+var JX123 = _0x42e2('0x11');
+var 浆糊 = _0x42e2('0x2a');
+var parwix = _0x42e2('0x1a');
+var 全民 = _0x42e2('0x9');
+var yunbo = _0x42e2('0x21');
+function setParse() {
+    var _0xe6f1f3 = {
+        'KhPPZ': function (_0x1a25a4, _0x21e0f4, _0x370e09) {
+            return _0x1a25a4(_0x21e0f4, _0x370e09);
+        },
+        'LlXCO': function (_0x3feb92, _0x28b97c) {
+            return _0x3feb92(_0x28b97c);
+        },
+        'vGdCr': function (_0x1a995e, _0xc1241b, _0x18f202) {
+            return _0x1a995e(_0xc1241b, _0x18f202);
+        },
+        'PXKvU': function (_0x323d84) {
+            return _0x323d84();
+        },
+        'vMNLw': function (_0xfa19cf, _0x5936dc) {
+            return _0xfa19cf(_0x5936dc);
+        },
+        'KuAXP': _0x42e2('0x25'),
+        'nZnLi': _0x42e2('0x19'),
+        'fORgu': function (_0x47648e, _0xa025ae) {
+            return _0x47648e == _0xa025ae;
+        },
+        'PTycl': _0x42e2('0x2c'),
+        'hTbcg': _0x42e2('0xa'),
+        'xFYyo': _0x42e2('0x1d'),
+        'AFYvJ': _0x42e2('0x4'),
+        'ahJCL': function (_0x1cc845, _0x1fdbd7) {
+            return _0x1cc845(_0x1fdbd7);
+        },
+        'kiiew': 'parseUpdate',
+        'rlMQN': function (_0x36484b) {
+            return _0x36484b();
+        },
+        'qiKRe': function (_0x3ec529) {
+            return _0x3ec529();
+        },
+        'MPTTU': _0x42e2('0x16')
+    };
+    function _0x14ea6d() {
+        var _0x3bd98a = fetch(_mySet['cj']);
+        _0xe6f1f3[_0x42e2('0xc')](writeFile, _mySet[_0x42e2('0x28')], _0x3bd98a);
+    }
+    function _0x53e7f5() {
+        var _0x1c8fbf = _0xe6f1f3[_0x42e2('0xd')](fetch, _mySet['x5Url']);
+        _0xe6f1f3['vGdCr'](writeFile, _mySet[_0x42e2('0x26')], _0x1c8fbf);
+    }
+    if (!_0xe6f1f3[_0x42e2('0xd')](fetch, _mySet[_0x42e2('0x28')])) {
+        _0xe6f1f3[_0x42e2('0xf')](_0x14ea6d);
+    }
+    if (!_0xe6f1f3['vMNLw'](fetch, _mySet[_0x42e2('0x26')])) {
+        _0xe6f1f3['PXKvU'](_0x53e7f5);
+    }
+    d[_0x42e2('0x8')]({
+        'desc': _0xe6f1f3[_0x42e2('0x1b')],
+        'url': _mySet[_0x42e2('0x1e')],
+        'col_type': _0xe6f1f3[_0x42e2('0x24')]
+    });
+    _0xe6f1f3[_0x42e2('0x14')](putVar, _0x42e2('0x10'), mySelect);
+    if (_0xe6f1f3[_0x42e2('0x0')](getVar(_0xe6f1f3['PTycl']), '开')) {
+        d[_0x42e2('0x8')]({
+            'img': 'https://ftp.bmp.ovh/imgs/2021/02/552388403275fd6f.jpg',
+            'title': _0xe6f1f3[_0x42e2('0x22')],
+            'desc': _0xe6f1f3[_0x42e2('0x2')],
+            'url': _0x42e2('0x29'),
+            'col_type': _0xe6f1f3[_0x42e2('0xb')]
+        });
+        if (!_0xe6f1f3[_0x42e2('0x12')](getVar, _0xe6f1f3[_0x42e2('0x5')])) {
+            _0xe6f1f3[_0x42e2('0x1')](_0x14ea6d);
+            _0xe6f1f3[_0x42e2('0x13')](_0x53e7f5);
+            _0xe6f1f3[_0x42e2('0x14')](putVar, _0xe6f1f3[_0x42e2('0x5')], _0xe6f1f3[_0x42e2('0x2b')]);
+        }
+    }
+}
+function toLazy(str){
+    try{
+        var oldLazy = 'var newLazy=' + str;
+        if(str.indexOf('http') != -1){
+            var newLazy = '"'+str+'"+input';
+        }else{eval(oldLazy);}
+        return newLazy;
+    }catch(e){
+        return '';
+    }
+}
+if(mySet.qju=='默认'){
+    var lazy = `if(input.indexOf('qq.com')!=-1){`+toLazy(mySet.tx)+`}else if(input.indexOf('iqiyi.com')!=-1){`+toLazy(mySet.qy)+`}else if(input.indexOf('youku.com')!=-1){`+toLazy(mySet.yk)+`}else if(input.indexOf('mgtv.com')!=-1){`+toLazy(mySet.mg)+`}else{`+toLazy(mySet.oth)+`}`;
+}else{
+    var lazy = toLazy(mySet.qju);
+}
